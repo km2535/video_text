9ff7c6ab2be1bf129df49409555a2283d0c096a9
km2535 <km2535@naver.com>
2021-12-19 07:35:52 +0900

    first

---
+++ main.js
@@ -0,0 +1,14 @@
+const dot = document.querySelector('.dot');
+const sec = document.querySelector('.sec');
+    dot.onclick = function(){
+        dot.classList.toggle('active')
+        sec.classList.toggle('active')
+    }
+
+document.addEventListener("mousemove", function(e){
+    let dot = document.querySelector('.dot');
+    dot.style.left = e.pageX + "px";
+    dot.style.top = e.pageY + "px";
+})
+
+// let content = document.querySelector('.content');
